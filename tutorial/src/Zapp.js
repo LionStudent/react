44eb99e06d68e8cfbadc3758eb251411db4ab9d9
lionstudent <lion.student@protonmail.com>
2020-06-01 04:52:42 +0000

    Doing some exercises...

---
+++ tutorial/src/Zapp.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Zapp(){
+    return (
+        <div>
+            <h1>Family</h1>
+            <p>This is Kwesi's family:</p>
+            <ul>
+                <li>Thema</li>
+                <li>Ife</li>
+                <li>Ayo</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Zapp
